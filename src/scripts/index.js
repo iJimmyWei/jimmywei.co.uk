e1a20a85c4b2e108e218bec871dea7b4c3570795
Jimmy Wei <ijimmywei@gmail.com>
2019-03-13 17:59:06 +0000

    Add in animated skillset progress bars

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,11 +14,37 @@ $(document).ready(function() {
   
       return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
     }
+
     // If element is scrolled into view, fade it in
     $(window).scroll(function() {
       $('.animated', '#block_two_container').each(function() {
         if (isScrolledIntoView(this) === true) {
-          $(this).addClass('fadeInLeft visible');
+            if (this.hasAnimated === undefined){
+                $(this).addClass('fadeInLeft visible');
+            }
+            this.hasAnimated = true;
+        }
+      });
+    });
+
+    // Animate the progress bars
+    $(window).scroll(function() {
+      $('.bar_container', '#block_three_container').each(function() {
+        let percentage = ($(this).find('.bar_percentage_label').text()).replace('%', '');
+        let progressBar = $(this).find('.bar_progress');
+
+        // Maximum percentage of the bar itself is 63% width
+        // Scale all values against this max width
+        let percentageView = Math.floor((percentage / 100) * 63);
+
+        if (isScrolledIntoView(this) === true) {
+            if (this.hasAnimated === undefined){
+                progressBar.animate({
+                    width: percentageView + '%'
+                }, 1200);
+            }
+            this.hasAnimated = true;
+        //   $(this).addClass('animated fadeInLeft visible');
         }
       });
     });
